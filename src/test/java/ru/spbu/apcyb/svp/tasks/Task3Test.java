415b8041bae0f20a121214ed0d7c31b9ed078edd
eertree <baskirill.an@gmail.com>
2022-09-07 21:52:05 +0300

    update tests

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,5 +1,8 @@
 package ru.spbu.apcyb.svp.tasks;
 
+/**
+ * Тесты для задания 3.
+ */
 public class Task3Test {
 
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
@@ -1,5 +1,8 @@
 package ru.spbu.apcyb.svp.tasks;
 
+/**
+ * Тесты для задания 5.
+ */
 public class Task5Test {
 
 }
