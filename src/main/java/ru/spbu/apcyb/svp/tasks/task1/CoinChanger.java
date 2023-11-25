e39dcd7a2e07954fc286c10bbaca7679ab9f29ec
BingoBy <yangibaevnn@gmail.com>
2023-11-25 11:58:51 +0300

    Added tests to CoinChanger

--- src/main/java/ru/spbu/apcyb/svp/tasks/task1/CoinChanger.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task1/CoinChanger.java
@@ -1,13 +1,9 @@
 package ru.spbu.apcyb.svp.tasks.task1;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.*;
 
 public class CoinChanger {
-
     private int sum;
     private int[] coins;
     private static final Logger LOGGER = Logger.getLogger(CoinChanger.class.getName());
@@ -16,17 +12,29 @@ public class CoinChanger {
         try (Scanner input = new Scanner(System.in)) {
 
             LOGGER.info("Enter amount of money for change: ");
-            sum = input.nextInt();
+            if (input.hasNextLine()) {
+                String userInput = input.nextLine();
 
-            LOGGER.info("Enter number of available denominations for change: ");
-            int numCoins = input.nextInt();
-            coins = new int[numCoins];
+                if (!userInput.isEmpty()) {
+                    sum = Integer.parseInt(userInput);
 
-            LOGGER.info("Enter available denominations: ");
-            for (int i = 0; i < numCoins; i++) {
-                coins[i] = input.nextInt();
+                    if (sum < 0) {
+                        LOGGER.info("Invalid amount. Please enter a non-negative integer.");
+                        input.nextInt();
+                    }
+
+                    LOGGER.info("Enter number of available denominations for change: ");
+                    int numCoins = input.nextInt();
+                    coins = new int[numCoins];
+
+                    LOGGER.info("Enter available denominations: ");
+                    for (int i = 0; i < numCoins; i++) {
+                        coins[i] = input.nextInt();
+                    }
+                } else {
+                    LOGGER.warning("Input cannot be empty.");
+                }
             }
-
         } catch (InputMismatchException e) {
             LOGGER.log(Level.WARNING, "Invalid input. Please enter a valid integer.", e);
         }
@@ -51,7 +59,7 @@ public class CoinChanger {
     private List<List<Integer>> getChangeOptions(int amount, int[] coins, int index) {
         List<List<Integer>> combinations = new ArrayList<>();
         if (amount == 0) {
-            combinations.add(new ArrayList<>()); // Добавляем пустую комбинацию (нет сдачи)
+            combinations.add(new ArrayList<>());
             return combinations;
         }
 
@@ -61,12 +69,10 @@ public class CoinChanger {
 
         int currentCoin = coins[index];
 
-        // Находим комбинации с учетом текущей монеты
         for (int i = 0; i * currentCoin <= amount; i++) {
             int remainingAmount = amount - i * currentCoin;
             List<List<Integer>> subCombinations = getChangeOptions(remainingAmount, coins, index + 1);
 
-            // Добавляем комбинации с учетом текущей монеты
             for (List<Integer> subCombination : subCombinations) {
                 for (int j = 0; j < i; j++) {
                     subCombination.add(currentCoin);
@@ -74,10 +80,8 @@ public class CoinChanger {
                 combinations.add(new ArrayList<>(subCombination));
             }
         }
-
         return combinations;
     }
-
     public static void main(String[] args) {
         CoinChanger changeMachine = new CoinChanger();
         int count = changeMachine.countChangeOptions();
---
+++ src/test/java/ru/spbu/apcyb/svp/tasks/task1/CoinChangerTest.java
@@ -0,0 +1,58 @@
+package ru.spbu.apcyb.svp.tasks.task1;
+
+import org.junit.jupiter.api.*;
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CoinChangerTest {
+    private final InputStream sysIn = System.in;
+    private final PrintStream sysOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private CoinChanger changer;
+
+    @BeforeEach
+    void prepare() {
+        changer = new CoinChanger();
+    }
+
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    @DisplayName("Test countChangeOptions with valid input")
+    void testCountChangeOptionsWithValidInput() {
+        assertCountChangeOptions("5\n3 2", 1);
+        assertCountChangeOptions("4\n2 1", 3);
+        assertCountChangeOptions("19\n3 1", 7);
+        assertCountChangeOptions("0\n3 2 1", 1);
+        assertCountChangeOptions("10\n10", 1);
+        assertCountChangeOptions("10\n0", 0);
+    }
+
+    @Test
+    @DisplayName("Test countChangeOptions with invalid input")
+    void testCountChangeOptionsWithInvalidInput() {
+        assertInvalidInputHandling("abc\n", "Invalid input. Please enter a valid integer.");
+        assertInvalidInputHandling("-10\n2 1", "Invalid amount. Please enter a non-negative integer.");
+        assertInvalidInputHandling("", "Input cannot be empty.");
+    }
+
+    private void assertCountChangeOptions(String input, int expectedOutput) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertEquals(expectedOutput, changer.countChangeOptions());
+    }
+
+    private void assertInvalidInputHandling(String input, String expectedErrorMessage) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertEquals(expectedErrorMessage, outContent.toString().trim());
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setIn(sysIn);
+        System.setOut(sysOut);
+    }
+}
